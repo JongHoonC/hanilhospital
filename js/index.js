f192aa281beeb36e81910db8bed4346e2f144b3f
JongHoonC <bjhlove0126@naver.com>
2023-01-16 14:02:23 +0900

    sdfsdf

--- js/index.js
+++ js/index.js
@@ -1,9 +1,14 @@
-// =================header drop down======================
-function dp_menu() {
-  let click = document.getElementById('drop-content');
-  if (click.style.display === 'none') {
-    click.style.display = 'block';
-  } else {
-    click.style.display = 'none';
-  }
+////////////////아티클 박스들////////////////////////////
+const articleBox = document.querySelectorAll('.article__box');
+const articleBoxImg = document.querySelectorAll('.article__box--img');
+
+for (let i = 0; i < articleBox.length; i++) {
+  articleBox[i].addEventListener('mouseover', e => {
+    articleBox[i].classList.add('active');
+    articleBoxImg[i].src = `../img/fav-box-icon${[i + 1]}.png`;
+  });
+  articleBox[i].addEventListener('mouseleave', e => {
+    articleBox[i].classList.remove('active');
+    articleBoxImg[i].src = `../img/fav-box-icon${[i + 1]}_black.png`;
+  });
 }

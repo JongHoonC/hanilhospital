d3fc2f6ddc4239ca188e050431cf2159b62a177e
JongHoonC <bjhlove0126@naver.com>
2023-02-03 10:41:25 +0900

    index.js 사진 경로 수정

--- js/index.js
+++ js/index.js
@@ -5,10 +5,10 @@ const articleBoxImg = document.querySelectorAll('.article__box--img');
 for (let i = 0; i < articleBox.length; i++) {
   articleBox[i].addEventListener('mouseover', e => {
     articleBox[i].classList.add('active');
-    articleBoxImg[i].src = `../img/fav-box-icon${[i + 1]}.png`;
+    articleBoxImg[i].src = `./img/fav-box-icon${[i + 1]}.png`;
   });
   articleBox[i].addEventListener('mouseleave', e => {
     articleBox[i].classList.remove('active');
-    articleBoxImg[i].src = `../img/fav-box-icon${[i + 1]}_black.png`;
+    articleBoxImg[i].src = `./img/fav-box-icon${[i + 1]}_black.png`;
   });
 }
